d6f5fa4e6d91eafb26dd24344545d3c39f7ea0f0
jeevesh <kjeevesh2001@gmail.com>
2023-05-30 11:17:34 +0530

    simple cronjob

---
+++ cron.py
@@ -0,0 +1,21 @@
+import time
+import subprocess
+from crontab import CronTab
+
+interval = int(input("Enter time interval"))
+
+#with open('/home/jeevesh/input.txt','r') as file:
+#	interval=int(file.readline().strip())
+cron = CronTab(user=True)
+
+command = f'python3 /home/jeevesh/hello.py'
+job = cron.new(command=command)
+
+job.every(interval)
+
+cron.write()
+
+while True:
+	subprocess.run(['python3','hello.py'], check=True)
+	
+	time.sleep(interval)
